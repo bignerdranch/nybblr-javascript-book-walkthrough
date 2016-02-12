4ccdbca6a597965f72c9f1f83856ad881f9f3314
Jonathan Martin <me@nybblr.com>
2016-02-12 14:01:23 -0500

    Chapter 15.

---
+++ chattrbox/websockets-server.js
@@ -0,0 +1,23 @@
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server;
+var port = 3001;
+var ws = new WebSocketServer({
+  port: port
+});
+var messages = [];
+console.log('websockets server started');
+ws.on('connection', function(socket) {
+  console.log('client connection established');
+
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
+
+  socket.on('message', function(data) {
+    console.log('message received: ' + data);
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data)
+    });
+    socket.send(data);
+  });
+});
