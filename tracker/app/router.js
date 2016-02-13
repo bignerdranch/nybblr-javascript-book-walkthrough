3a5baeb67203898700d7bb531cdd74156a75cb37
Jonathan Martin <me@nybblr.com>
2016-02-13 12:14:23 -0600

    Chapter 19.

--- tracker/app/router.js
+++ tracker/app/router.js
@@ -6,6 +6,16 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('sightings', function() {
+    this.route('new');
+  });
+  this.route('sighting', function() {
+    this.route('edit');
+  });
+  this.route('cryptids');
+  this.route('cryptid');
+  this.route('witnesses');
+  this.route('witness');
 });
 
 export default Router;
