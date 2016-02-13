d0adbf5bb69f8826110d660464b900f8c265635b
Jonathan Martin <me@nybblr.com>
2016-02-13 13:26:40 -0600

    Chapter 21.

--- tracker/config/environment.js
+++ tracker/config/environment.js
@@ -16,6 +16,24 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'self'",
+      // Allow scripts from live reload
+      'script-src': "'self' ws://localhost:49152",
+      // Add external font uri
+      'font-src': "'self'",
+      // Allow data (ajax/websocket) from course api https://bnr-tracker-api.herokuapp.com
+      // Allow scripts from live reload
+      'connect-src': "'self' https://bnr-tracker-api.herokuapp.com ws://localhost:49152",
+      // Allow images from the origin itself (i.e. current domain)
+      'img-src': "'self'",
+      // Allow inline styles
+      'style-src': "'self' 'unsafe-inline'",
+      // `media-src` will be omitted from policy
+      // Browser will fallback to default-src for media resources (which is to deny, see above).
+      'media-src': null
     }
   };
 
@@ -25,6 +43,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['ember-cli-mirage'] = {
+      enabled: false
+    };
   }
 
   if (environment === 'test') {
