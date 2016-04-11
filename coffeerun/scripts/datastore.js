cb5d0f63816e5279c08e29b3163b3427e535552e
Jonathan Martin <me@nybblr.com>
2016-04-10 20:32:47 -0400

    Chapter 7.

---
+++ coffeerun/scripts/datastore.js
@@ -0,0 +1,24 @@
+(function(window) {
+  'use strict';
+  // code will go here
+  var App = window.App || {};
+
+  function DataStore() {
+    console.log('running the DataStore function');
+    this.data = {};
+  }
+  DataStore.prototype.add = function(key, val) {
+    this.data[key] = val;
+  };
+  DataStore.prototype.get = function(key) {
+    return this.data[key];
+  };
+  DataStore.prototype.getAll = function() {
+    return this.data;
+  };
+  DataStore.prototype.remove = function(key) {
+    delete this.data[key];
+  };
+  App.DataStore = DataStore;
+  window.App = App;
+})(window);
