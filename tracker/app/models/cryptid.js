e15efc650a3ce6743db82b5b0623bfe563412e6d
Jonathan Martin <me@nybblr.com>
2016-04-13 14:49:19 -0400

    Chapter 20.

---
+++ tracker/app/models/cryptid.js
@@ -0,0 +1,12 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import {
+  hasMany
+} from 'ember-data/relationships';
+
+export default Model.extend({
+  name: attr('string'),
+  cryptidType: attr('string'),
+  profileImg: attr('string'),
+  sightings: hasMany('sighting')
+});
---
+++ tracker/app/models/witness.js
@@ -0,0 +1,15 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+import {
+  hasMany
+} from 'ember-data/relationships';
+
+export default Model.extend({
+  fName: attr('string'),
+  lName: attr('string'),
+  email: attr('string'),
+  sightings: hasMany('sighting'),
+  fullName: Ember.computed('fName', 'lName', function() {
+    return this.get('fName') + ' ' + this.get('lName');
+  })
+});
