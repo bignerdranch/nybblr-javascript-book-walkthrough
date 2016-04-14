8a76a5a0a3846d2187dc71eae425122546457264
Jonathan Martin <me@nybblr.com>
2016-04-14 14:24:12 -0400

    Chapter 23.

--- tracker/app/routes/sightings.js
+++ tracker/app/routes/sightings.js
@@ -2,18 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let record1 = this.store.createRecord('sighting', {
-      location: 'Atlanta',
-      sightedAt: new Date('2012-10-24')
+    return this.store.findAll('sighting', {
+      reload: true
     });
-    let record2 = this.store.createRecord('sighting', {
-      location: 'Calloway',
-      sightedAt: new Date('2012-09-24')
-    });
-    let record3 = this.store.createRecord('sighting', {
-      location: 'Asilomar',
-      sightedAt: new Date('2012-12-24')
-    });
-    return [record1, record2, record3];
   }
 });
--- tracker/app/routes/sightings/new.js
+++ tracker/app/routes/sightings/new.js
@@ -1,4 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    var newSighting = this.store.createRecord('sighting');
+    var cryptids = this.store.findAll('cryptid');
+    var witnesses = this.store.findAll('witness');
+    return Ember.RSVP.hash({
+      sighting: newSighting,
+      cryptids: cryptids,
+      witnesses: witnesses
+    });
+  },
+  sighting: Ember.computed.alias('controller.model.sighting'),
+  actions: {
+    willTransition() {
+      var sighting = this.get('controller.model.sighting');
+      if (sighting.get('hasDirtyAttributes')) {
+        sighting.deleteRecord();
+      }
+    },
+    create() {
+      var self = this;
+      this.get('sighting').save().then(function() {
+        self.transitionTo('sightings');
+      });
+    },
+    cancel() {
+      this.get('sighting').deleteRecord();
+      this.transitionTo('sightings');
+    }
+  }
 });
