934d10645dda80be366a7d70062278996ee9072e
Jonathan Martin <me@nybblr.com>
2016-07-15 12:22:46 -0500

    Chapter 12.

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -24,6 +24,19 @@
       this.elements[0].focus();
     });
   };
+  FormHandler.prototype.addInputHandler = function(fn) {
+    console.log('Setting input handler for form');
+    this.$formElement.on('input', '[name="emailAddress"]', function(event) {
+      var emailAddress = event.target.value;
+      var message = '';
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity('');
+      } else {
+        message = emailAddress + ' is not an authorized email address!'
+        event.target.setCustomValidity(message);
+      }
+    });
+  };
   App.FormHandler = FormHandler;
   window.App = App;
 })(window);
--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -6,6 +6,7 @@
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
   var CheckList = App.CheckList;
   var myTruck = new Truck('ncc-1701', new DataStore());
   window.myTruck = myTruck;
@@ -16,5 +17,6 @@
     myTruck.createOrder.call(myTruck, data);
     checkList.addRow.call(checkList, data);
   });
+  formHandler.addInputHandler(Validation.isCompanyEmail);
   console.log(formHandler);
 })(window);
