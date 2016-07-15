3e52c80cbbf2b4bc9ed8de4c1ab9fc7566d8d2ed
Jonathan Martin <me@nybblr.com>
2016-07-15 13:18:48 -0500

    Chapter 16.

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
+var wss = require('./websockets-server');
 var handleError = function(err, res) {
   res.writeHead(404);
   res.end();
