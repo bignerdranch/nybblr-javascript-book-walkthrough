bae326b2943adbc3fde7513014c7b1e6db4c5350
Jonathan Martin <me@nybblr.com>
2016-07-20 20:37:45 -0400

    Chapter 24.

--- tracker/app/routes/sightings.js
+++ tracker/app/routes/sightings.js
@@ -2,18 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let record1 = this.store.createRecord('sighting', {
-      location: 'Atlanta',
-      sightedAt: new Date('2016-02-09')
+    return this.store.findAll('sighting', {
+      reload: true
     });
-    let record2 = this.store.createRecord('sighting', {
-      location: 'Calloway',
-      sightedAt: new Date('2016-03-14')
-    });
-    let record3 = this.store.createRecord('sighting', {
-      location: 'Asilomar',
-      sightedAt: new Date('2016-03-21')
-    });
-    return [record1, record2, record3];
   }
 });
