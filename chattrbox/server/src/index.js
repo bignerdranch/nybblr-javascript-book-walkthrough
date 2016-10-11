5a4f0e16286a850a526f789248c0a4159430e16d
Jonathan Martin <me@nybblr.com>
2016-10-11 13:58:07 -0400

    Use koa-static middleware to serve files.
    $ npm install koa-static@next --save

--- chattrbox/server/src/index.js
+++ chattrbox/server/src/index.js
@@ -1,23 +1,13 @@
 var http = require('http');
-var fs = require('fs');
-var zlib = require('zlib');
-var path = require('path');
-var extract = require('./extract');
 var wss = require('./websockets-server');
 
 var Koa = require('koa');
-var send = require('koa-send');
 var compress = require('koa-compress');
+var serve = require('koa-static');
 var app = new Koa();
 
 app.use(compress());
-app.use(async ctx => {
-  console.log('Responding to a request.');
-  var filePath = extract(ctx.request.url);
-  await send(ctx, filePath, {
-    root: path.resolve(__dirname, '../../app')
-  });
-});
+app.use(serve('./app'));
 
 var server = http.createServer(app.callback());
 
