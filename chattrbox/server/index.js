1b4337fa0e004117eaba8d90827d536e2204c565
Jonathan Martin <me@nybblr.com>
2016-10-11 11:16:36 -0400

    Build pipeline for server code.
    $ npm install babel-cli --save-dev
    $ npm install babel-register --save-dev
    $ npm install babel-polyfill --save
    $ npm install babel-preset-es2015 --save-dev
    $ npm install babel-preset-es2016 --save-dev
    $ npm install babel-preset-es2017 --save-dev

---
+++ chattrbox/server/index.js
@@ -0,0 +1,9 @@
+const env = process.env.NODE_ENV || 'development';
+const src = env === 'production' ? './dist' : './src';
+
+require('babel-polyfill');
+if (env === 'development') {
+  require('babel-register');
+}
+
+require(src);
