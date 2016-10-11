14718ab428149ec50e0aeea65d4eb88c41193b5c
Jonathan Martin <me@nybblr.com>
2016-10-11 15:05:10 -0400

    Load environment variables from .env in development.
    $ npm install dotenv --save

--- chattrbox/server/index.js
+++ chattrbox/server/index.js
@@ -4,6 +4,7 @@ const src = env === 'production' ? './dist' : './src';
 require('babel-polyfill');
 if (env === 'development') {
   require('babel-register');
+  require('dotenv').config();
 }
 
 require(src);
