215655121a4ae829bd7ce7c6a2b6d13e8c055f1b
Jonathan Martin <me@nybblr.com>
2016-10-11 09:16:03 -0400

    Hello, Koa.
    $ npm install koa@next --save

--- chattrbox/index.js
+++ chattrbox/index.js
@@ -2,20 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
-var handleError = function(err, res) {
-  res.writeHead(404);
-  res.end();
-};
-var server = http.createServer(function(req, res) {
-  console.log('Responding to a request.');
-  var filePath = extract(req.url);
-  fs.readFile(filePath, function(err, data) {
-    if (err) {
-      handleError(err, res);
-      return;
-    } else {
-      res.end(data);
-    }
-  });
+
+var Koa = require('koa');
+var app = new Koa();
+
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
 });
+
+var server = http.createServer(app.callback());
+
 server.listen(3000);
